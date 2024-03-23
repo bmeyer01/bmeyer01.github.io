019f8d810d1eaff59512d475df6b0e0ee9675791
Ben Meyer <bmeyer01@gmail.com>
2024-03-22 18:29:34 -0700

    Update bulb.js

--- bulb.js
+++ bulb.js
@@ -75,7 +75,7 @@ function connect() {
 	console.log('Requesting Bluetooth Device...');
 	navigator.bluetooth.requestDevice({
 			filters: [{
-				services: [6E400001-B5A3-F393-E0A9-E50E24DCCA9E]
+				name: 'PicoW'
 			}]
 		})
 		.then(device => {
@@ -85,11 +85,11 @@ function connect() {
 		})
 		.then(server => {
 			console.log('Getting Service 0xffe5 - Light control...');
-			return server.getPrimaryService(6E400003-B5A3-F393-E0A9-E50E24DCCA9E);
+			return server.getPrimaryService('PicoW');
 		})
 		.then(service => {
 			console.log('Getting Characteristic 0xffe9 - Light control...');
-			return service.getCharacteristic(6E400002-B5A3-F393-E0A9-E50E24DCCA9E);
+			return service.getCharacteristic('PicoW');
 		})
 		.then(characteristic => {
 

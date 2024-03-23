1ce47d653216e8a079be62b5a3cba3b251d112c4
Ben Meyer <bmeyer01@gmail.com>
2024-03-22 19:38:47 -0700

    Update bulb.js

--- bulb.js
+++ bulb.js
@@ -85,7 +85,7 @@ function connect() {
 		})
 		.then(server => {
 			console.log('Getting Service 0xffe5 - Light control...');
-			return server.getPrimaryService('6E400003-B5A3-F393-E0A9-E50E24DCCA9E');
+			return server.getPrimaryService('6E400001-B5A3-F393-E0A9-E50E24DCCA9E');
 		})
 		.then(service => {
 			console.log('Getting Characteristic 0xffe9 - Light control...');

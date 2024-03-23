c8c00441ddd173bd55106f42a3dd2c4d42e7c5d0
Ben Meyer <bmeyer01@gmail.com>
2024-03-22 18:03:33 -0700

    Update bulb.js

--- bulb.js
+++ bulb.js
@@ -75,7 +75,7 @@ function connect() {
 	console.log('Requesting Bluetooth Device...');
 	navigator.bluetooth.requestDevice({
 			filters: [{
-				services: [0xffe5]
+				name: [PicoW]
 			}]
 		})
 		.then(device => {

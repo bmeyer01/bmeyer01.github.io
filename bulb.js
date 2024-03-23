930970f0b390ed97079b2e6ac22b02b65886d6dd
Ben Meyer <bmeyer01@gmail.com>
2024-03-22 21:08:43 -0700

    update

--- bulb.js
+++ bulb.js
@@ -64,9 +64,9 @@ function updateColor() {
 
 function onConnected() {
 	document.querySelector('.connect-button').classList.add('hidden');
-	document.querySelector('.connect-another').classList.remove('hidden');
+	//document.querySelector('.connect-another').classList.remove('hidden');
 	document.querySelector('.wheel').classList.remove('hidden');
-	document.querySelector('.mic-button').classList.remove('hidden');
+	//document.querySelector('.mic-button').classList.remove('hidden');
 	document.querySelector('.power-button').classList.remove('hidden');
 	turnedOn = false;
 }
@@ -75,7 +75,7 @@ function connect() {
 	console.log('Requesting Bluetooth Device...');
 	navigator.bluetooth.requestDevice({
 			filters: [{
-				services: ['6E400001-B5A3-F393-E0A9-E50E24DCCA9E']
+				services: [0xffe5]
 			}]
 		})
 		.then(device => {
@@ -85,11 +85,11 @@ function connect() {
 		})
 		.then(server => {
 			console.log('Getting Service 0xffe5 - Light control...');
-			return server.getPrimaryService('6E400003-B5A3-F393-E0A9-E50E24DCCA9E');
+			return server.getPrimaryService(0xffe5);
 		})
 		.then(service => {
 			console.log('Getting Characteristic 0xffe9 - Light control...');
-			return service.getCharacteristic('6E400002-B5A3-F393-E0A9-E50E24DCCA9E');
+			return service.getCharacteristic(0xffe9);
 		})
 		.then(characteristic => {
 
